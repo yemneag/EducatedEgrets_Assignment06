4b6b58f99a3fefab5a021be25fb7a6529ee08b17
Abel Yemaneab <yemaneag@mail.uc.edu>
2025-02-26 18:25:28 -0500

    Updated main.py with CupPackage object

--- EducatedEgrets_Assignment06/mainPackage/main.py
+++ EducatedEgrets_Assignment06/mainPackage/main.py
@@ -1,4 +1,5 @@
 # main.py
+
 # File Name : EducatedEgrets_Assignment06
 # Student Name: Abel Yemaneab
 # email:  yemaneag@mail.uc.edu
@@ -6,28 +7,33 @@
 # Due Date:   02/35/2025
 # Course #/Section:   IS4010-001
 # Semester/Year:   Spring 2025
-# Brief Description of the assignment:  {required}
+# Brief Description of the assignment:  
 
-# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
-# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+# Brief Description of what this module does. 
+# Citations: https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/ 
 
 # Anything else that's relevant:
 
+from CupsPackage.Cups import *
 from bookPackage.book import *
 from ShirtsPackage.Shirts import *
 
 if __name__ == "__main__":
-    #Book Section
+
+    # This project simulates a bookstore and the objects you might find in it
+
+    # Book Section
+
     # Instantiate a Book 
     book1 = Book("Fiction", 20, 10)
     book2 = Book("Non-Fiction", 25, 5)
-    # Printing Book Info
+    # Printing Initial Book Info
     print("Initial Book Details:")
     print(book1)
     print(book2)
     print("")
 
-    #Getter
+    # Getter
     print(f"Book1 is a {book1.get_type()} book costing ${book1.get_price()} and we have {book1.get_quantity()} in stock.") 
     print(f"Book2 is a {book2.get_type()} book costing ${book2.get_price()} and we have {book2.get_quantity()} in stock.") 
     print("")
@@ -43,6 +49,7 @@ if __name__ == "__main__":
     print(book1)
     print(book2)
     print("")
+
     # Display book selling functionality
     book1.sell_books(4)
     print(book1)
@@ -55,17 +62,18 @@ if __name__ == "__main__":
     print(book1)
     print(book2)
     print("")
+
     # Shirt Section
  
     # Instantiate a Shirt
     shirt1 = Shirt("Adidas", "L", "Blue", 35.99)
 
-    #Printing Shirt Info
+    #Printing Initial Shirt Info
     print("Initial Shirt Details")
     print(shirt1)
     print("")
     
-    #get shirt info using getters
+    # get shirt info using getters
     print("Shirt Details Using Getters:")
     print(f"This shirt is a {shirt1.brand} shirt, being size {shirt1.size}, its color is {shirt1.color}, and it costs ${shirt1.price}")
 
@@ -75,7 +83,7 @@ if __name__ == "__main__":
     shirt1.change_color("Green")
     print("")
     
-    #Display Update
+    # Display Update
     print("Updated Shirt Details:")
     print(shirt1)
     print("")
@@ -88,3 +96,38 @@ if __name__ == "__main__":
     # Print final details
     print("Final Shirt Details:")
     print(shirt1)
+    print("")
+
+    # Cup Section
+
+    # Instantiate a Cup
+    cup1 = Cup("Plastic", 200, 9.99)
+
+    #Printing Initial Cup Info
+    print("Initial Cup Details:")
+    print(cup1)
+    print("")
+
+    # get cup info using getters
+    print("Shirt Details Using Getters:")
+    print(f"This cup is made of {cup1.material}, it can hold {cup1.capacity}ml, and it costs ${cup1.price}")
+    print("")
+
+    # updating cup properties with setters
+    cup1.material = "Ceramic"
+    cup1.capacity = 1000
+    cup1.price = 99.99
+
+    # updated cup properties
+    print("Updated Cup Properties:")
+    print(cup1)
+    print("")
+
+    # Filling Cup
+    print("Filling Cup Up:")
+    print(cup1.fill())
+    print("")
+
+    # Print final Cup Details
+    print("Final Cup Details:")
+    print(cup1)
